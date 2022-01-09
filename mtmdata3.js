4518868d13ccc22ad45d699983fa057ce2bae10d
yukinami504 <95061705+yukinami504@users.noreply.github.com>
2022-01-10 04:24:11 +0900

    update some files

---
+++ mtmdata3.js
@@ -0,0 +1,79 @@
+let mtmdata_3=[
+  {
+    "id": 8001,
+    "name": "ある少女の記憶 博麗霊夢",
+    "rare": "r",
+    "character": "博麗霊夢",
+    "attr": "em",
+    "release_date": "2021/08/03",
+    "status": {
+      "max_st": [
+        3807,
+        1593,
+        2765
+      ],
+      "min_st": [
+        1302,
+        416,
+        885
+      ],
+      "burst1": null,
+      "burst2": null,
+      "effect": {
+        "cat": "score",
+        "num": [
+          [
+            8,
+            43,
+            4,
+            12
+          ],
+          [
+            8,
+            53,
+            5.6,
+            13
+          ],
+          [
+            8,
+            63,
+            7.2,
+            15
+          ]
+        ]
+      },
+      "odekake": {
+        "cat": "time",
+        "num": [
+          8,
+          9,
+          10
+        ]
+      }
+    }
+  },
+  {
+    "id": 0,
+    "name": null,
+    "rare": null,
+    "character": null,
+    "attr": null,
+    "release_date": "2021/08/03",
+    "status": {
+      "max_st": [
+        0,
+        0,
+        0
+      ],
+      "min_st": [
+        0,
+        0,
+        0
+      ],
+      "burst1": null,
+      "burst2": null,
+      "effect": null,
+      "odekake": null
+    }
+  }
+];
